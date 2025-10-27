42a1f45fc040d95599f01ca7c2f3932e5ec44907
wky21 <wongsta92@gmail.com>
2025-10-26 18:47:09 -0700

    chore: add S3 backend for remote state

---
+++ terraform/environments/test/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "wky21-eks-tf-state"
+    key            = "terraform/environments/test/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-locks"
+    encrypt        = true
+  }
+}
