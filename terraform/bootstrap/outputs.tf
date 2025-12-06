64da6a57cb722cce4a8b5294ebddcfd86931b1eb
wky21 <wongsta92@gmail.com>
2025-12-05 18:13:12 -0800

    update outputs

--- terraform/bootstrap/outputs.tf
+++ terraform/bootstrap/outputs.tf
@@ -1,9 +1,9 @@
 output "bootstrap_s3_bucket" {
   description = "S3 bucket name for Terraform state"
-  value       = aws_s3_bucket.tf_state.bucket
+  value       = aws_s3_bucket.terraform_state.bucket
 }
 
 output "bootstrap_dynamodb_table" {
   description = "DynamoDB table name for Terraform locking"
-  value       = aws_dynamodb_table.tf_lock.name
+  value       = aws_dynamodb_table.terraform_lock.name
 }
